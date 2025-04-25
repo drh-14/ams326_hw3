dae82eb0b226c7f88fa98f433a73b322c295ce9c
drh-14 <darrenh1134@gmail.com>
2025-04-25 17:02:06 -0400

    finished Q2

--- hw3.py
+++ hw3.py
@@ -38,35 +38,29 @@ class Q1:
         plt.show()
         
 class Q2:
-    def __init__(self, T_initial, step_size):
-        self.x = 0
-        self.y = 0
+    def __init__(self, T, step_size):
         self.alpha = 0
-        self.rectangle = [[0.5, 1 / (2 * np.sqrt(2))], [-0.5, 1 / (2 * np.sqrt(2))], [0.5, -1 / (2 * np.sqrt(2))], [-0.5, -1 / (2 * np.sqrt(2))]]
+        self.x = random.uniform(-0.8, 0.8)
+        self.y = random.uniform(-0.8, 0.8)
+        self.rectangle = [[0.5 + self.x, 1 / (2 * np.sqrt(2)) + self.y], [-0.5 + self.x, 1 / (2 * np.sqrt(2)) + self.y],
+                          [0.5 + self.x, -1 / (2 * np.sqrt(2)) + self.y], [-0.5 + self.x, -1 / (2 * np.sqrt(2)) + self.y]]
         self.step_size = step_size
-        self.T_initial = T_initial
+        self.T = T
         
-    def translate_rectangle(self, dx, dy):
-        new_rectangle = self.rectangle.copy()
-        for i in range(len(new_rectangle)):
-            new_rectangle[i][0] += dx
-            new_rectangle[i][1] += dy
-        return new_rectangle 
-         
-    def rotate_rectangle(self, alpha):
-        new_rectangle = self.rectangle.copy()
-        for i in range(len(self.new_rectangle)):
-            offset_x = self.center[0]
-            offset_y = self.center[1]
-            new_rectangle[i][0] -= offset_x
-            new_rectangle[i][1] -= offset_y
-            vector = np.array(new_rectangle[i])
-            rotation_matrix = np.array([[np.cos(alpha), -np.sin(alpha)], [np.sin(alpha), np.cos(alpha)]])
+    def generate_new_rectangle(self, state):
+        x,y,alpha = state
+        rectangle = [[0.5, 1/ (2 * np.sqrt(2))], [0.5, -1/ (2 * np.sqrt(2))], [-0.5,  1/ (2 * np.sqrt(2))], [-0.5, - 1/ (2 * np.sqrt(2))]]
+        rotation_matrix = np.array([[np.cos(alpha), -np.sin(alpha)], [np.sin(alpha), np.cos(alpha)]])
+        for i in range(len(rectangle)):
+            rectangle[i][0] -= x
+            rectangle[i][1] -= y
+            vector = np.array([rectangle[i][0], rectangle[i][1]])
             rotated_vector = np.linalg.matmul(rotation_matrix, vector)
-            new_rectangle[i][0] = rotated_vector[0] + offset_x
-            new_rectangle[i][1] = rotated_vector[1] = offset_y
-        return new_rectangle
-            
+            new_x, new_y = rotated_vector[0] + x, rotated_vector[1] + y
+            rectangle[i][0] = new_x
+            rectangle[i][1] = new_y
+        return rectangle
+        
     def inside_curve(self, x, y):
         return np.power((np.square(x) + np.square(y)), 3) <= 4 * np.square(x) * np.square(y)
     
@@ -75,42 +69,43 @@ class Q2:
         min_y, max_y = min(coordinates[1] for coordinates in rectangle), max(coordinates[1] for coordinates in rectangle)
         return (min_x <= x <= max_x) and (min_y <= y <= max_y)
     
-    def compute_area(self, rectangle, N=10000):
+    def compute_area(self, rectangle, N=100000):
         hits = 0
         for _ in range(N):
             x,y = random.uniform(-1, 1), random.uniform(-1, 1)
-            if self.inside_curve(x,y) and self.inside_rectangle(x,y):
+            if self.inside_curve(x,y) and self.inside_rectangle(x,y, rectangle):
                 hits += 1
-        return (hits / N) * (1 / np.sqrt(2))
+        return (hits / N) * 4
     
     def temperature_schedule(self, k):
-        return self.T_initial / k
+        return self.T / k
     
     def get_new_state(self):
-        x_adjust = random.uniform(-self.step_size, self.step_size),
-        y_adjust = self.y + random.uniform(-self.step_size, self.step_size)
-        alpha_adjust = self.alpha + random.uniform(-self.step_size, self.step_size)
+        return [self.x + random.uniform(-self.step_size, self.step_size), self.y + random.uniform(-self.step_size, self.step_size), self.alpha + random.uniform(-self.step_size, self.step_size)]
         
-    def get_probability(self, new_state, old_state, T):
-        return min(1, np.exp(-((self.compute_area(new_state) - self.compute_area(old_state)) / T)))
+    def get_probability(self, new_rectangle, T):
+        return min(1, np.exp(-((self.compute_area(new_rectangle) - self.compute_area(self.rectangle)) / T)))
     
     def solve(self):
         k = 1
-        while True:
-            curr_state = [self.x, self.y, self.alpha]
+        while self.T >= 10 ** -5:
             new_state = self.get_new_state()
-            p = self.get_probability(new_state, curr_state, self.temperature_schedule(k))
-            #if probability simulation true
-            new_x, new_y, new_alpha = new_state
-            self.x, self.y, self.alpha = new_x, new_y, new_alpha
+            new_rectangle = self.generate_new_rectangle(new_state)
+            p = self.get_probability(new_rectangle, self.T)
+            accepted_new_state = random.randint(0, int(100 * p)) < int(100 * p)
+            if accepted_new_state:
+                new_x, new_y, new_alpha = new_state
+                self.x, self.y, self.alpha = new_x, new_y, new_alpha
+                self.rectangle = self.generate_new_rectangle([self.x, self.y, self.alpha])
             k += 1
-        # At the end, return area from final state
+            self.T = self.temperature_schedule(k)
+        print(f"Optimal Center: ({self.x}, {self.y}), Optimal Angle: {self.alpha}, Optimal Area: {self.compute_area(self.rectangle)}")
             
 class Q3:
     def __init__(self):
         self.diffeq = lambda x,y: (y / x) - 0.5 * np.sqrt(1 + np.pow(y/x, 2))
-        self.h = 0.001
-        
+        self.h = 0.01
+                
     def solve(self):
         x = 100
         y = 0
@@ -127,9 +122,9 @@ class Q3:
         plt.show()
 
 if __name__ == "__main__":
-    Q1 = Q1(10000, 0, 100,  0, 100)
-    Q1.solve()
-    Q2 = Q2(100, 0.0001)
-    print(Q2.solve())
-    Q3 = Q3()
-    Q3.solve()
+    #Q1 = Q1(4444444, 0, 100, 0, 100)
+    #Q1.solve()
+    #Q3 = Q3()
+    #Q3.solve()
+    Q2 = Q2(100, 0.01)
+    Q2.solve()
